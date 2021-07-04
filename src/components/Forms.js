3d3acf9ad46fa54b382a5e06f4661d445d7d8bb1
Tuan Anh <manhhungit.37@gmail.com>
2021-07-04 10:40:47 +0700

    form

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -8,15 +8,32 @@ function Forms() {
   //   firstName: ''
   // })
   const [firstName, setFirstName] = useState('')
+  const [form, setForm] = useState({
+    learn: "react",
+    age: 30
+  })
 
   function onChange(event) {
     setFirstName(event.target.value)
+  }
+
+  function onChangeForm(event) {
+    const name = event.target.name;
+    setForm({
+      ...form,
+      [name]: event.target.value
+    })
   }
 
   console.log('firstName: ', firstName)
   return (
     <div>
       <Input value={firstName} onChange={onChange} />
+
+      <h4>Multiple Input</h4>
+      <input type="text" name="learn" value={form.learn} onChange={onChangeForm} />
+      <input type="text" name="age" value={form.age} onChange={onChangeForm} />
+
     </div>
   )
 }
